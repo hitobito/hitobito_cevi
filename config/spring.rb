baa0d6a52873a63f0e9e1684fa4486698db0da09
Mathis Hofer <hofer@puzzle.ch>
2014-05-07 14:01:38 +0200

    add license headers to source files

--- config/spring.rb
+++ config/spring.rb
@@ -1,1 +1,8 @@
+# encoding: utf-8
+
+#  Copyright (c) 2012-2014, CEVI Regionalverband ZH-SH-GL. This file is part of
+#  hitobito_cevi and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_cevi.
+
 Spring.application_root="../hitobito"
