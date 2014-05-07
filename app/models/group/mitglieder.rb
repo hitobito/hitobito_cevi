36f83be15200a488a584cb8460df02a8b3c343c5
Mathis Hofer <hofer@puzzle.ch>
2014-05-07 14:00:06 +0200

    annotate source files with schema information

--- app/models/group/mitglieder.rb
+++ app/models/group/mitglieder.rb
@@ -1,2 +1,28 @@
+# == Schema Information
+#
+# Table name: groups
+#
+#  id             :integer          not null, primary key
+#  parent_id      :integer
+#  lft            :integer
+#  rgt            :integer
+#  name           :string(255)      not null
+#  short_name     :string(31)
+#  type           :string(255)      not null
+#  email          :string(255)
+#  address        :string(1024)
+#  zip_code       :integer
+#  town           :string(255)
+#  country        :string(255)
+#  contact_id     :integer
+#  created_at     :datetime
+#  updated_at     :datetime
+#  deleted_at     :datetime
+#  layer_group_id :integer
+#  creator_id     :integer
+#  updater_id     :integer
+#  deleter_id     :integer
+#
+
 # Abstract Mitglieder
 class Group::Mitglieder < Group; end
