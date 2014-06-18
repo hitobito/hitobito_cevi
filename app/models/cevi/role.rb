b9b516b4aea30862b2fe5d8ec16d3346b9ad7ab6
Andreas Maierhofer <maierhofer@puzzle.ch>
2014-06-18 14:20:12 +0200

    add role spec, refs #6871

--- app/models/cevi/role.rb
+++ app/models/cevi/role.rb
@@ -1,4 +1,18 @@
 # encoding: utf-8
+# == Schema Information
+#
+# Table name: roles
+#
+#  id         :integer          not null, primary key
+#  person_id  :integer          not null
+#  group_id   :integer          not null
+#  type       :string(255)      not null
+#  label      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#  deleted_at :datetime
+#
+
 
 #  Copyright (c) 2012-2014, CEVI Regionalverband ZH-SH-GL. This file is part of
 #  hitobito_cevi and licensed under the Affero General Public License version 3
---
+++ spec/models/role_spec.rb
@@ -0,0 +1,62 @@
+# encoding: utf-8
+
+#  Copyright (c) 2012-2014, CEVI Regionalverband ZH-SH-GL. This file is part of
+#  hitobito_cevi and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_cevi.
+
+# == Schema Information
+#
+# Table name: roles
+#
+#  id         :integer          not null, primary key
+#  person_id  :integer          not null
+#  group_id   :integer          not null
+#  type       :string(255)      not null
+#  label      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#  deleted_at :datetime
+#
+
+
+
+require 'spec_helper'
+
+describe Role do
+
+  describe Group::DachverbandExterne::Externer do
+    subject {  Group::DachverbandExterne::Externer }
+    it { should_not be_visible_from_above }
+  end
+
+  describe Group::JungscharExterne::Externer do
+    subject {  Group::JungscharExterne::Externer }
+    it { should_not be_visible_from_above }
+  end
+
+  describe Group::MitgliederorganisationExterne::Externer do
+    subject {  Group::MitgliederorganisationExterne::Externer }
+    it { should_not be_visible_from_above }
+  end
+
+  describe Group::SportExterne::Externer do
+    subject {  Group::SportExterne::Externer }
+    it { should_not be_visible_from_above }
+  end
+
+  describe Group::TenSingExterne::Externer do
+    subject {  Group::TenSingExterne::Externer }
+    it { should_not be_visible_from_above }
+  end
+
+  describe Group::VereinExterne::Externer do
+    subject {  Group::VereinExterne::Externer }
+    it { should_not be_visible_from_above }
+  end
+
+  describe Group::WeitereArbeitsgebieteExterne::Externer do
+    subject {  Group::WeitereArbeitsgebieteExterne::Externer }
+    it { should_not be_visible_from_above }
+  end
+end
