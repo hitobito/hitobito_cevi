59b6f63a8e107b173239b051b6ce4faf8def8ec5
Andreas Maierhofer <a.maierhofer@gmail.com>
2019-12-11 15:27:20 +0100

    Add missing spec for spender filtering
    refs hitobito/hitobito_cevi#39

---
+++ spec/domain/person/filter/list_spec.rb
@@ -0,0 +1,64 @@
+# encoding: utf-8
+
+#  Copyright (c) 2012-2019, CEVI Regionalverband ZH-SH-GL. This file is part of
+#  hitobito_cevi and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_cevi.
+
+require 'spec_helper'
+
+describe Person::Filter::List do
+  let(:dachverband) { groups(:dachverband) }
+  let(:bulei)       { people(:bulei) }
+
+  before do
+    @spenders = Fabricate(Group::DachverbandSpender.sti_name, parent: dachverband)
+    @spender  = Fabricate(Group::DachverbandSpender::Spender.sti_name, group: @spenders).person
+    Fabricate(Group::Dachverband::Administrator.sti_name, group: dachverband, person: @spender)
+  end
+
+  def self.each_range
+    [nil, 'deep', 'layer'].each do |range|
+      yield range
+    end
+  end
+
+
+  context Group::Dachverband::Administrator do
+    each_range do |range|
+      it "#{range} hides other spender role" do
+        filtered = filter(range: range)
+        expect(filtered.entries).to be_empty
+        expect(filtered.all_count).to eq 1
+      end
+    end
+  end
+
+  context Group::DachverbandSpender::Spender do
+    before { Fabricate(Group::DachverbandSpender::Spender.sti_name, group: @spenders, person: bulei) }
+
+    each_range do |range|
+      it "#{range} hides other spender role, shows own spender role" do
+        filtered = filter(range: range)
+        expect(filtered.entries).to match_array([bulei])
+        expect(filtered.all_count).to eq 2
+      end
+    end
+  end
+
+  context Group::DachverbandSpender::SpendenVerwalter do
+    before { Fabricate(Group::DachverbandSpender::SpendenVerwalter.sti_name, group: @spenders, person: bulei) }
+
+    each_range do |range|
+      it "#{range} shows other spender role" do
+        filtered = filter(range: range)
+        expect(filtered.entries).to match_array([@spender, bulei])
+        expect(filtered.all_count).to eq 2
+      end
+    end
+  end
+
+  def filter(range: nil, group: @spenders, filter: PeopleFilter.new(name: 'name'))
+    Person::Filter::List.new(group, bulei, filter)
+  end
+end
