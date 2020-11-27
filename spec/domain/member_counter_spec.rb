eea52ba9867b9bfcea61a53f34389f281582da4b
Matthias Viehweger <viehweger@puzzle.ch>
2020-11-27 15:14:17 +0100

    Conform to linter and styleguide

--- spec/domain/member_counter_spec.rb
+++ spec/domain/member_counter_spec.rb
@@ -1,6 +1,6 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
-#  Copyright (c) 2012-2014, CEVI Regionalverband ZH-SH-GL. This file is part of
+#  Copyright (c) 2012-2020, CEVI Regionalverband ZH-SH-GL. This file is part of
 #  hitobito_cevi and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_cevi.
