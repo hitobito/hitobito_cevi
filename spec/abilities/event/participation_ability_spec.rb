1e6d8b424ec86c5861dce7144ad093af1c652e79
Matthias Viehweger <viehweger@puzzle.ch>
2020-12-09 15:12:34 +0100

    Change whitespace, indentation or line-breaks

--- spec/abilities/event/participation_ability_spec.rb
+++ spec/abilities/event/participation_ability_spec.rb
@@ -10,7 +10,8 @@ require 'spec_helper'
 describe Event::ParticipationAbility do
 
   def build(attrs)
-    course = Event::Course.new(groups: attrs.fetch(:groups).map { |g| groups(g) }, tentative_applications: true)
+    course = Event::Course.new(groups: attrs.fetch(:groups).map { |g| groups(g) },
+                               tentative_applications: true)
     @participation = Event::Participation.new(event: course, person: attrs[:person])
   end
 
@@ -20,17 +21,19 @@ describe Event::ParticipationAbility do
   subject { Ability.new(user) }
 
   context 'participation with person' do
-
     context 'layer_and_below_full' do
-      let(:role) { Fabricate(Group::MitgliederorganisationGeschaeftsstelle::Geschaeftsleiter.name, group: groups(:zhshgl_gs)) }
+      let(:role) { Fabricate(Group::MitgliederorganisationGeschaeftsstelle::Geschaeftsleiter.name,
+                             group: groups(:zhshgl_gs)) }
 
       it 'may not create_tentative for person in upper layer' do
-        person = Fabricate(Group::DachverbandGeschaeftsstelle::Geschaeftsleiter.name, group: groups(:dachverband_gs)).person
+        person = Fabricate(Group::DachverbandGeschaeftsstelle::Geschaeftsleiter.name,
+                           group: groups(:dachverband_gs)).person
         is_expected.not_to be_able_to(:create_tentative, build(groups: [:zhshgl], person: person))
       end
 
       it 'may create_tentative for person in his layer' do
-        person = Fabricate(Group::MitgliederorganisationGremium::Mitglied.name, group: groups(:zhshgl_beirat)).person
+        person = Fabricate(Group::MitgliederorganisationGremium::Mitglied.name,
+                           group: groups(:zhshgl_beirat)).person
         is_expected.to be_able_to(:create_tentative, build(groups: [:zhshgl], person: person))
       end
 
@@ -54,7 +57,8 @@ describe Event::ParticipationAbility do
       let(:role) { Fabricate(Group::Ortsgruppe::AdministratorCeviDB.name, group: groups(:stadtzh)) }
 
       it 'may not create_tentative for person in upper layer' do
-        person = Fabricate(Group::DachverbandGeschaeftsstelle::Geschaeftsleiter.name, group: groups(:dachverband_gs)).person
+        person = Fabricate(Group::DachverbandGeschaeftsstelle::Geschaeftsleiter.name,
+                           group: groups(:dachverband_gs)).person
         is_expected.not_to be_able_to(:create_tentative, build(groups: [:zhshgl], person: person))
       end
 
@@ -71,7 +75,8 @@ describe Event::ParticipationAbility do
   end
 
   context 'Ausbildungsmitglied' do
-    let(:role) { Fabricate(Group::MitgliederorganisationGremium::Ausbildungsmitglied.name.to_sym, group: groups(:zhshgl_beirat)) }
+    let(:role) { Fabricate(Group::MitgliederorganisationGremium::Ausbildungsmitglied.name.to_sym,
+                           group: groups(:zhshgl_beirat)) }
 
     it 'may show participant in same layer course' do
       participation = Fabricate(:event_participation, event: events(:top_course))
@@ -79,12 +84,14 @@ describe Event::ParticipationAbility do
     end
 
     it 'may not show participant in same layer event' do
-      participation = Fabricate(:event_participation, event: Fabricate(:event, groups: [groups(:zhshgl_beirat)]))
+      participation = Fabricate(:event_participation,
+                                event: Fabricate(:event, groups: [groups(:zhshgl_beirat)]))
       is_expected.not_to be_able_to(:show, participation)
     end
 
     it 'may not show participant in lower layer event' do
-      participation = Fabricate(:event_participation, event: Fabricate(:event, groups: [groups(:stadtzh)]))
+      participation = Fabricate(:event_participation,
+                                event: Fabricate(:event, groups: [groups(:stadtzh)]))
       is_expected.not_to be_able_to(:show, participation)
     end
 
