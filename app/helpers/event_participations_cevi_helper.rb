c16ea2dba7f8524e8c23590883c93175a51e7a28
Matthias Viehweger <viehweger@puzzle.ch>
2020-12-17 22:49:21 +0100

    Allow leaders to promote all participants to leaders

--- app/helpers/event_participations_cevi_helper.rb
+++ app/helpers/event_participations_cevi_helper.rb
@@ -1,6 +1,6 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
-#  Copyright (c) 2012-2014, CEVI Regionalverband ZH-SH-GL. This file is part of
+#  Copyright (c) 2012-2020, CEVI Regionalverband ZH-SH-GL. This file is part of
 #  hitobito_cevi and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_cevi.
@@ -13,6 +13,12 @@ module EventParticipationsCeviHelper
 
   def format_event_participation_payed(participation)
     participation.payed? ? t('global.yes') : t('global.no')
+  end
+
+  def new_course_leader_role_dropdown(group, part)
+    Dropdown::Event::RoleAdd.new(self, group, part.event, part.person).tap do |dropdown|
+      dropdown.label = t(:'dropdown/event/role_add.add_leader')
+    end
   end
 
 end
