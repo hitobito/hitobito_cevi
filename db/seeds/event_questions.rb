682c8f8e12f967be60ab5a9c323237bde324f0dc
Matthias Viehweger <viehweger@puzzle.ch>
2021-01-11 12:33:38 +0100

    Workaround for seedfu/globalize inability
    SeedFu does not see the translated attributes, ActiveRecord does.
    Therefore we solve this 'manually'ish.

--- db/seeds/event_questions.rb
+++ db/seeds/event_questions.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2020, CEVI Regionalverband ZH-SH-GL. This file is part of
+#  Copyright (c) 2020-2021, CEVI Regionalverband ZH-SH-GL. This file is part of
 #  hitobito_cevi and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_cevi.
@@ -12,11 +12,18 @@ role_names = Event::Course.role_types
 
 # if we want to change the wording, we can do this with a
 # data-migration alongside the same update here.
-Event::Question.seed(:event_id, :question,
+[
   {
     event_id: nil, # global question
     question: 'Ich habe Interesse an einer Mitarbeit im Leiterteam in einer der folgenden Rollen',
     choices: role_names.join(','),
     multiple_choices: true
   },
-)
+].each do |attrs|
+  eq = Event::Question.find_or_initialize_by(
+    event_id: attrs.delete(:event_id),
+    question: attrs.delete(:question),
+  )
+  eq.attributes = attrs
+  eq.save!
+end
