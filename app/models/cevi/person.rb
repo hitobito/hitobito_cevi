734a8903eebc59bbc6e77451f9b5f0eb290d09bf
Patrick Uhlmann <git@uhlme.ch>
2023-03-25 15:59:27 +0100

    remove schema information comments
    they are mostly outdated and it is better to consult the schema
    directly.

--- app/models/cevi/person.rb
+++ app/models/cevi/person.rb
@@ -1,62 +1,9 @@
 # encoding: utf-8
 
-#  Copyright (c) 2012-2017, CEVI Regionalverband ZH-SH-GL. This file is part of
+#  Copyright (c) 2023, Cevi.DB Steuergruppe. This file is part of
 #  hitobito_cevi and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_cevi.
-
-# == Schema Information
-#
-# Table name: people
-#
-#  id                      :integer          not null, primary key
-#  first_name              :string(255)
-#  last_name               :string(255)
-#  company_name            :string(255)
-#  nickname                :string(255)
-#  company                 :boolean          default(FALSE), not null
-#  email                   :string(255)
-#  address                 :string(1024)
-#  zip_code                :integer
-#  town                    :string(255)
-#  country                 :string(255)
-#  gender                  :string(1)
-#  birthday                :date
-#  additional_information  :text
-#  contact_data_visible    :boolean          default(FALSE), not null
-#  created_at              :datetime
-#  updated_at              :datetime
-#  encrypted_password      :string(255)
-#  reset_password_token    :string(255)
-#  reset_password_sent_at  :datetime
-#  remember_created_at     :datetime
-#  sign_in_count           :integer          default(0)
-#  current_sign_in_at      :datetime
-#  last_sign_in_at         :datetime
-#  current_sign_in_ip      :string(255)
-#  last_sign_in_ip         :string(255)
-#  picture                 :string(255)
-#  last_label_format_id    :integer
-#  creator_id              :integer
-#  updater_id              :integer
-#  primary_group_id        :integer
-#  failed_attempts         :integer          default(0)
-#  locked_at               :datetime
-#  title                   :string(255)
-#  profession              :string(255)
-#  j_s_number              :integer
-#  joined                  :date
-#  ahv_number              :string(255)
-#  ahv_number_old          :string(255)
-#  nationality             :string(255)
-#  salutation_parents      :string(255)
-#  name_parents            :string(255)
-#  member_card_number      :integer
-#  salutation              :string(255)
-#  canton                  :string(255)
-#  confession              :string(255)
-#  correspondence_language :string(255)
-#
 
 module Cevi::Person
   extend ActiveSupport::Concern
--- app/models/group/vorstand.rb
+++ app/models/group/vorstand.rb
@@ -1,35 +1,9 @@
 # encoding: utf-8
 
-#  Copyright (c) 2012-2014, CEVI Regionalverband ZH-SH-GL. This file is part of
+#  Copyright (c) 2023, Cevi.DB Steuergruppe. This file is part of
 #  hitobito_cevi and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_cevi.
-# == Schema Information
-#
-# Table name: groups
-#
-#  id             :integer          not null, primary key
-#  parent_id      :integer
-#  lft            :integer
-#  rgt            :integer
-#  name           :string(255)      not null
-#  short_name     :string(31)
-#  type           :string(255)      not null
-#  email          :string(255)
-#  address        :string(1024)
-#  zip_code       :integer
-#  town           :string(255)
-#  country        :string(255)
-#  contact_id     :integer
-#  created_at     :datetime
-#  updated_at     :datetime
-#  deleted_at     :datetime
-#  layer_group_id :integer
-#  creator_id     :integer
-#  updater_id     :integer
-#  deleter_id     :integer
-#  founding_date  :date
-#
 
 # Abstract Vorstand
 class Group::Vorstand < Group; end
