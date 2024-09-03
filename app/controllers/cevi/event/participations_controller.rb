83c504bd9b22f04c3a96b4a2dd91b5425d36d80a
Matthias Viehweger <viehweger@puzzle.ch>
2024-09-03 14:53:32 +0200

    Remove unneeded conditional, mysql is not supported anymore

--- app/controllers/cevi/event/participations_controller.rb
+++ app/controllers/cevi/event/participations_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2012-2020, CEVI Regionalverband ZH-SH-GL. This file is part of
+#  Copyright (c) 2012-2024, CEVI Regionalverband ZH-SH-GL. This file is part of
 #  hitobito_cevi and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_cevi.
@@ -12,11 +12,7 @@ module Cevi
 
       included do
         sort_mappings_with_indifferent_access["ortsgruppe"] =
-          if /mysql/.match?(::Event.connection.adapter_name.downcase)
-            'CONCAT_WS("", groups.short_name, groups.name)'
-          else
-            "(COALESCE(groups.short_name, '') || groups.name)"
-          end
+          "(COALESCE(groups.short_name, '') || groups.name)"
 
         alias_method_chain :assign_attributes, :check
       end
