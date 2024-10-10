cc10d13120a35b57f9d1eb18f4ccde9cc4cb5474
Niklas Jäggi <jaeggi@puzzle.ch>
2024-10-10 15:19:57 +0200

    Fix rubocop

--- app/helpers/cevi/event_participations_helper.rb
+++ app/helpers/cevi/event_participations_helper.rb
@@ -1,14 +1,14 @@
--#  Copyright (c) 2012-2024, CEVI Regionalverband ZH-SH-GL. This file is part of
--#  hitobito_cevi and licensed under the Affero General Public License version 3
--#  or later. See the COPYING file at the top-level directory or at
--#  https://github.com/hitobito/hitobito_cevi.
+#  Copyright (c) 2012-2024, CEVI Regionalverband ZH-SH-GL. This file is part of
+#  hitobito_cevi and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_cevi.
 
 module Cevi::EventParticipationsHelper
-  def event_participation_table_options(t)
+  def event_participation_table_options(t, event)
     t.col(Person.human_attribute_name(:ortsgruppe)) do |p|
       format_ortsgruppe(p)
     end
-    if can?(:update, @event)
+    if can?(:update, event)
       t.attr(:internal_comment)
       t.sortable_attr(:payed)
     end
