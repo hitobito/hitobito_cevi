f2e15e47d6e82966f8f7300fdd33edd802606848
Matthias Viehweger <viehweger@puzzle.ch>
2024-11-18 23:32:58 +0100

    Reload some schema-info

---
+++ spec/support/reload_column_information.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2019-2024, Puzzle ITC. This file is part of
+#  hitobito_cevi and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_cevi.
+
+[
+  Event,
+  Group,
+  Person
+].map(&:reset_column_information)
