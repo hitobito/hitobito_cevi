ad6221a165bba4205920111680f79a4c555c2bca
carlobeltrame <carlo@beltra.me>
2026-09-08 17:07:59 +0200

    Reimplement member count editing with a proper modern nested form
    Follow-up from hitobito/hitobito@f0db205c and hitobito/hitobito#3751
    Fixes hitobito/hitobito#4470

--- app/helpers/member_count_helper.rb
+++ app/helpers/member_count_helper.rb
@@ -4,17 +4,6 @@
 #  https://github.com/hitobito/hitobito_cevi.
 
 module MemberCountHelper
-  def member_counts_fields_blueprint(group)
-    fields_for("additional_member_counts[]", group.member_counts.build,
-      builder: StandardFormBuilder) do |f|
-      content_tag(:tr) do
-        content_tag(:td, f.input_field(:born_in, class: "span1")) +
-          content_tag(:td, f.input_field(:person_f, class: "span1")) +
-          content_tag(:td, f.input_field(:person_m, class: "span1"))
-      end
-    end
-  end
-
   def format_member_count_born_in(member_count)
     member_count.born_in || "unbekannt"
   end
